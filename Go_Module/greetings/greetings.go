f264a7a3d3ad40f024739367c91d4e3aa6cfd91c
LoydNgei <loydngei@gmail.com>
2024-07-14 19:46:24 +0300

    Added how to handle greetings to multiple people

--- Go_Module/greetings/greetings.go
+++ Go_Module/greetings/greetings.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person
@@ -11,18 +12,41 @@ import (
 func Hello(name string) (string, error) {
 
 	if name == "" {
-		return "", errors.New("empty name")
+		return name, errors.New("empty name")
 	}
+
+	rand.Seed(time.Now().UnixNano())
 
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Hellos returns a map that associates each of the named people with a greeting message
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	// Loop through the received slice of names, calling the Hello
+	// function to get a message for each name
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		// In the map, associate the retrieved message with the name
+
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 // RandomFormat returns one of a set of greeting messages. The returned message is selected at random
 
 func randomFormat() string {
 	formats := []string{
-		"Hi, %v. Welcome",
+		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
 		"Hail, %v! well met!",
 	}
