6ece88fe30647d6513cb6c82535d49f918f80242
LoydNgei <loydngei@gmail.com>
2024-07-24 20:30:00 +0300

    GoTour

---
+++ GoTour/Packages/packages.go
@@ -0,0 +1,21 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+// )
+
+// func main() {
+// 	fmt.Println("My favorite number is", rand.Intn(10))
+// }
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
